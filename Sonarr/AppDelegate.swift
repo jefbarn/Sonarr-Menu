fd332420f2801cce75c419889a4e0b3766206a56
Jeff Barnes <jeff.barnes@gmail.com>
2014-12-29 21:42:10 -0900

    Initial Commit

---
+++ Sonarr/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Sonarr
+//
+//  Created by Jeff Barnes on 12/29/14.
+//  Copyright (c) 2014 Sonarr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
