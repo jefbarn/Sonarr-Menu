e3ee51aee72a68cef9c2fa6485b2831f537395d2
Jeff Barnes <jeff.barnes@gmail.com>
2015-10-22 00:06:02 -0800

    Initial Commit

---
+++ Sonarr-Menu/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Sonarr-Menu
+//
+//  Created by Jeff Barnes on 10/22/15.
+//  Copyright © 2015 Sonarr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
